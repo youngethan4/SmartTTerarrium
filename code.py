ce4c00783b45b9d0e2c02f6884e86eb4a525f336
Ethan Young <Youngethan4@gmail.com>
2025-01-19 16:53:37 -0600

    Update to run the pump for x amount of seconds. Check humidity after x amount of seconds as well.

--- code.py
+++ code.py
@@ -7,6 +7,8 @@ import time
 
 WIDTH = 128
 HEIGHT = 64
+TIME_TO_PUMP = 10 # The amount of time in seconds to pump for
+CHECK_HUMIDITY_PERIOD = 20 # Amount of time in seconds to check the humidity at.
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
@@ -34,23 +36,59 @@ button_previous_state = button.value
 pump = digitalio.DigitalInOut(board.D8)
 pump.direction = digitalio.Direction.OUTPUT
 pump.value = False
+set_pumping = False
+pump_start_time = 0
 
-while True:
-    display.fill(0)
-    display.text('Humidity: {0}%'.format(getHumidity()), 0, 5, 1)
-    display.text('Temperature: {0}F'.format(getTemp()), 0, 20, 1)
-    button_current_state = button.value
-    if button_current_state != button_previous_state:
-        if not button.value:
-            print('Humidity: {0}%'.format(getHumidity()))
-            print('Temperature: {0}F'.format(getTemp()))
-            pump.value = not pump.value
-        else:
-            print("Button UP")
-    button_previous_state = button_current_state
-    pump_text = "NOT PUMPING"
-    if pump.value:
-        pump_text = "PUMPING"
-    display.text(pump_text, 0, 35, 1)
-    display.show()
-    time.sleep(.05)
+next_humidity_check = time.time() + CHECK_HUMIDITY_PERIOD
+
+try:
+    while True:
+        # Get humidity and temp
+        humidity = getHumidity()
+        temp = getTemp()
+
+        # If enough time has passed, check if the humidity if less than 50% and start pumping or reset the checking period
+        if next_humidity_check <= time.time():
+            if humidity < 50 and not pump.value:
+                set_pumping = True
+            else:
+                next_humidity_check = time.time() + CHECK_HUMIDITY_PERIOD
+
+        # Check if the button was pressed and start pumping if so
+        if button.value != button_previous_state and not button.value and not pump.value:
+            set_pumping = True
+        button_previous_state = button.value
+
+        # Configure display texts based on if we are pumping
+        # Stop pumping if time is up
+        pump_text = "Not pumping"
+        humidity_check_display = f'Next run: {int(int(next_humidity_check - time.time()) / 60)}m {int(int(next_humidity_check - time.time()) % 60)}s'
+        if set_pumping and not pump.value:
+            pump.value = True
+            pump_start_time = time.time()
+            set_pumping = False
+        if pump.value:
+            if pump_start_time + TIME_TO_PUMP > time.time():
+                pump_text = f"Pumping for {pump_start_time + TIME_TO_PUMP - time.time()}s."
+            else:
+                pump.value = False
+                next_humidity_check = time.time() + CHECK_HUMIDITY_PERIOD
+        
+        # Configure the display
+        display.fill(0)
+        display.text(f'Humidity: {humidity}%', 0, 5, 1)
+        display.text(f'Temperature: {temp}F', 0, 20, 1)
+        display.text(pump_text, 0, 35, 1)
+        if not pump.value:
+            display.text(humidity_check_display, 0, 50, 1)
+        display.show()
+        
+        # Sleep
+        time_to_sleep = 0.1
+        time.sleep(time_to_sleep)
+except Exception as e:
+    pump.value = False
+    print("THERE WAS AN ERROR!!!")
+    print(e)
+finally:
+    pump.value = False
